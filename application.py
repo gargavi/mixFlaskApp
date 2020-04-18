60c0c6fb94bee1f5cb79e08f7816ed45c0475603
Avi Garg <avigarg2000@gmail.com>
2020-04-18 00:21:03 -0700

    What to do?

--- application.py
+++ application.py
@@ -281,7 +281,7 @@ def get_prefs():
 
 
 #this just runs the function
-if __name__ == "__main__":
-    #application.run(debug = True, host = "0.0.0.0")
-    application.run(debug=True, port=PORT)
+#if __name__ == "__main__":
+#    #application.run(debug = True, host = "0.0.0.0")
+#    application.run(debug=True, port=PORT)
 
